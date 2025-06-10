0afaf0c1142d41846105bbf9cde236c5994db488
Christian Juric1 <business.juric1@gmail.com>
2025-06-10 11:22:00 +0200

    Added some utility classes (ObservableObject, RelayCommand).

---
+++ Xproject.UI/Utils/RelayCommand.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows.Input;
+
+namespace Xproject.UI.Utils;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object> _execute;
+    private readonly Predicate<object> _canExecute;
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public RelayCommand(Action execute, Func<bool> canExecute = null)
+        : this(
+            execute != null ? _ => execute() : null,
+            canExecute != null ? _ => canExecute() : null)
+    {}
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute?.Invoke(parameter) ?? true;
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute(parameter);
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CommandManager.InvalidateRequerySuggested();
+    }
+}
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> _execute;
+    private readonly Predicate<T> _canExecute;
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        if (parameter is T typedParameter)
+            return _canExecute?.Invoke(typedParameter) ?? true;
+
+        return _canExecute?.Invoke(default(T)) ?? true;
+    }
+
+    public void Execute(object parameter)
+    {
+        if (parameter is T typedParameter)
+            _execute(typedParameter);
+        else
+            _execute(default(T));
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CommandManager.InvalidateRequerySuggested();
+    }
+}
